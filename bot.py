d2d6bd8528226f983834457be492a3830fa071e3
miladafg1 <dlshksth952@gmail.com>
2026-05-30 18:30:34 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import os
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update, context):
+    await update.message.reply_text("ربات با موفقیت فعاله ❤️")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
